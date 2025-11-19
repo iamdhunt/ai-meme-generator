3c7138f8bec77f61073654f4ed5c63ebe1206c01
Dario <5452772+iamdhunt@users.noreply.github.com>
2025-11-18 23:39:20 -0700

    Imported meme templates and wired the data

---
+++ src/components/MemeGenerator.tsx
@@ -0,0 +1,10 @@
+export default function MemeGenerator() {
+  return (
+    <>
+      <h2>Meme Generator</h2>
+      <button type="button" disabled={true}>
+        Generate Meme
+      </button>
+    </>
+  );
+}
