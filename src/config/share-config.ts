9c5a9a0e387ebf2e315e42de9605279dd5b5e650
Dario <5452772+iamdhunt@users.noreply.github.com>
2025-12-01 23:11:41 -0700

    feat: simplify app description for clarity and conciseness

--- src/config/share-config.ts
+++ src/config/share-config.ts
@@ -1,4 +1,4 @@
 export const APP_URL = "https://mememachine3000.com";
 export const APP_NAME = "Infinite Meme Machine 3000";
 export const APP_DESCRIPTION =
-  "Infinite Meme Machine 3000 is a fully armed computational comedy reactor. Feed it vibes, choose your destiny, and watch it spit out premium internet nonsense at unsafe speeds. Use responsibly. Or don’t.";
+  "Fully armed computational comedy reactor. Feed it vibes, choose your destiny, and watch it spit out premium internet nonsense at unsafe speeds.";
